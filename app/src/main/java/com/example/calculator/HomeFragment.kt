481d71d46cbcc23c054f14c6716141f2f55d463a
Marina Flores <mlbtf@discente.ifpe.edu.br>
2023-03-29 21:05:23 -0300

    incluindo ListView para listar dados salvos no banco

--- app/src/main/java/com/example/calculator/HomeFragment.kt
+++ app/src/main/java/com/example/calculator/HomeFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.navigation.fragment.findNavController
+import androidx.room.Room
+import com.example.calculator.data.database.AppDatabase
+import com.example.calculator.data.entities.CalculationHistory
 import com.example.calculator.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
+    var db: AppDatabase? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,8 +24,20 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        this.db = Room.databaseBuilder(
+            requireContext().applicationContext,
+            AppDatabase::class.java, "item_database"
+        ).allowMainThreadQueries().build()
+
         val navController = findNavController()
         binding.btnHome.setOnClickListener { navController.navigate(R.id.action_homeFragment_to_calculatorScreen) }
+
+        val calculationHistoryDao = this.db!!.calculationHistoryDao();
+        val history = calculationHistoryDao.getAllCalculations()
+        val calculationHistoryList = binding.calculatorList as ListView
+        val adapter = ArrayAdapter<CalculationHistory>(requireContext(), android.R.layout.simple_list_item_1, history)
+
+        calculationHistoryList.adapter = adapter;
 
         return binding.root
     }
